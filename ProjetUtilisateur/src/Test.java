a1a0ef3c2e3c7dd651a1f8a014d9e7e67ffda026
AnisHDLY <anis.hedhli@gmail.com>
2020-01-13 18:59:29 +0100

    second

--- ProjetUtilisateur/src/Test.java
+++ ProjetUtilisateur/src/Test.java
@@ -1,8 +1,7 @@
-import Model.*;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println(new Personne("a", "b"));
+		System.out.println(new Personne("a", "b", ""));
 
 	}
 
